6b19e71843b38e0d15980b7a423426ba661df736
SpencasaurusRex <SpencasaurusRex@gmail.com>
2020-02-19 19:15:52 -0500

    Initial commit

---
+++ Assets/Scripts/ChunkCoord.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Components;
+using UnityEngine;
+
+public class ChunkCoord : IEquatable<ChunkCoord>
+{
+    public int X;
+    public int Y;
+    public int Z;
+
+    public ChunkCoord(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public static ChunkCoord FromBlockCoords(int gx, int gy, int gz) =>
+        new ChunkCoord(gx << Chunk.Bits, gy << Chunk.Bits, gz << Chunk.Bits);
+
+    public static ChunkCoord FromPosition(Vector3 position) =>
+        FromBlockCoords((int) position.x, (int) position.y, (int) position.z);
+
+    public bool Equals(ChunkCoord other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        return X == other.X && Y == other.Y && Z == other.Z;
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            var hashCode = X;
+            hashCode = (hashCode * 397) ^ Y;
+            hashCode = (hashCode * 397) ^ Z;
+            return hashCode;
+        }
+    }
+}
---
+++ Assets/Scripts/Components/Chunk.cs
@@ -0,0 +1,20 @@
+﻿namespace Components
+{
+    public class Chunk
+    {
+        public const int Bits = 4;
+        public const int Size = 1 << Bits;
+        public const int Volume = Size * Size * Size;
+
+        public bool Loaded;
+        public int[] Data;
+        public ChunkCoord Coord;
+
+        public Chunk(ChunkCoord coord)
+        {
+            Loaded = false;
+            Data = new int[Volume];
+            Coord = coord;
+        }
+    }
+}
